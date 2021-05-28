d7676cc40403620baaea4441deba22efff7970cd
adoniramjudson <adoni07.ramjudson@gmail.com>
2021-05-28 17:26:48 +0530

    made changes

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,27 +1,39 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
+import PropTypes from "prop-types";
 import facebook from "./../../img/facebook.png";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     number: "",
     password: "",
     password2: "",
+    travels: "",
+    gst: "",
   });
-  const { name, email, number, password, password2 } = formData;
+  const [person, setPerson] = useState(null);
+  const { name, email, number, password, password2, travels, gst } = formData;
 
   const onChange = (e) =>
     setFormData({
       ...formData,
       [e.target.getAttribute("name")]: e.target.value,
     });
+  const handleChange = (e) => {
+    // {[e.target.checked] : e.target.value}
+    setPerson(e.target.value);
+    //console.log({ [e.target.checked]: e.target.value });
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Password do not match");
+      setAlert("Password do not match", "danger");
     } else {
       console.log(formData);
     }
@@ -29,75 +41,173 @@ const Register = () => {
 
   return (
     <Fragment>
-      <h1 className="large text-primary">Sign Up</h1>
       <p className="lead">
         <i className="fas fa-user"></i> Create Your Account
       </p>
-      <form className="form" onSubmit={(e) => onSubmit(e)}>
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            value={name}
-            onChange={(e) => onChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="email"
-            placeholder="Email Address"
-            name="email"
-            value={email}
-            onChange={(e) => onChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="number"
-            placeholder="Mobile Number"
-            name="number"
-            value={number}
-            onChange={(e) => onChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={password}
-            onChange={(e) => onChange(e)}
-            minLength="6"
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="password2"
-            value={password2}
-            onChange={(e) => onChange(e)}
-            minLength="6"
-          />
-        </div>
-        <input type="submit" className="btn btn-primary" value="Register" />{" "}
-        <br />
-        <div>
-          {" "}
-          Login with <br />
-          <button className="btn social-acc-box" id="newFbId">
-            <div className="facebook-logo">
-              <img src={facebook} width="5" />
+
+      <h1 className="large text-primary">Sign Up</h1>
+      <label for="cust">Customer</label>
+      <br></br>
+      <input
+        type="radio"
+        id="cust"
+        name="type"
+        value="cust"
+        onChange={handleChange}
+      ></input>
+      <label for="travels">Travel Agency</label>
+      <br></br>
+
+      <input
+        type="radio"
+        id="travels"
+        name="type"
+        value="travels"
+        onChange={handleChange}
+      ></input>
+      <div>
+        {person == "cust" ? (
+          <form className="form" onSubmit={(e) => onSubmit(e)}>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={name}
+                onChange={(e) => onChange(e)}
+                required
+              />
             </div>
-            <div className="facebook-text">Facebook</div>
-          </button>
-          {/* <input type="submit" className="btn btn-primary" value="Facebook" /> */}
-        </div>
-      </form>
+            <div className="form-group">
+              <input
+                type="email"
+                placeholder="Email Address"
+                name="email"
+                value={email}
+                onChange={(e) => onChange(e)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="number"
+                placeholder="Mobile Number"
+                name="number"
+                value={number}
+                onChange={(e) => onChange(e)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={(e) => onChange(e)}
+                minLength="6"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                name="password2"
+                value={password2}
+                onChange={(e) => onChange(e)}
+                minLength="6"
+              />
+            </div>
+            <input type="submit" className="btn btn-primary" value="Register" />{" "}
+            <br />
+            <div>
+              {" "}
+              Login with <br />
+              <button className="btn social-acc-box" id="newFbId">
+                <div className="facebook-logo">
+                  <img src={facebook} width="5" />
+                </div>
+                <div className="facebook-text">Facebook</div>
+              </button>
+              {/* <input type="submit" className="btn btn-primary" value="Facebook" /> */}
+            </div>
+          </form>
+        ) : (
+          <form className="form" onSubmit={(e) => onSubmit(e)}>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={name}
+                onChange={(e) => onChange(e)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Travels Name"
+                name="travels"
+                value={travels}
+                onChange={(e) => onChange(e)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                placeholder="Business Email"
+                name="email"
+                value={email}
+                onChange={(e) => onChange(e)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="GSTIN"
+                name="gst"
+                value={gst}
+                onChange={(e) => onChange(e)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="number"
+                placeholder="Mobile Number"
+                name="number"
+                value={number}
+                onChange={(e) => onChange(e)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={(e) => onChange(e)}
+                minLength="6"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                name="password2"
+                value={password2}
+                onChange={(e) => onChange(e)}
+                minLength="6"
+              />
+            </div>
+          </form>
+        )}
+      </div>
+
       <p className="my-1">
         Already have an account? <Link to="login">Sign In</Link>
       </p>
@@ -105,4 +215,9 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.prototype = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert, register })(Register);
--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,89 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Landing = () => {
+  const [formData, setFormData] = useState({
+    from: "",
+    to: "",
+    date: "",
+  });
+
+  const { from, to, date } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Success");
+  };
   return (
     <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
           <h1 className="large">Welcome to Atlantic Travels</h1>
-          <section id="search">
-            <div className="clearfix search-wrap">
-              <div
-                className="fl search-box clearfix"
-                // style="border-color: rgb(210, 210, 210);"
-              >
-                <span className="fl icon-city icon"></span>
-                <div>
-                  <input
-                    type="text"
-                    id="src"
-                    className="db"
-                    data-message="Please enter city"
-                    tabindex="1"
-                    autocomplete="off"
-                  />
-                  <label
-                    for="src"
-                    className="db from move-up"
-                    // style="left:15%"
-                  >
-                    FROM
-                  </label>
-                  <div className="error-message-fixed "> </div>
-                </div>
-              </div>
-              <span className="icon-doublearrow icon" id="togglebtn"></span>
-              <div className="fl search-box">
-                <span
-                  className="fl icon-city icon"
-                  // style="margin-left: 13px;"
-                ></span>
-                <div>
-                  <input
-                    type="text"
-                    id="dest"
-                    className="db"
-                    data-message="Please enter a destination city"
-                    tabindex="2"
-                    autocomplete="off"
-                  />
-                  <label
-                    for="dest"
-                    //  style="left: 21%;"
-                    className="db to move-up"
-                  >
-                    TO
-                  </label>
-                  <div className="error-message-fixed "> </div>
-                </div>
-              </div>
-              <div
-                className="fl search-box date-box gtm-onwardCalendar"
-                // style="border-color: rgb(210, 210, 210);"
-              >
-                <span className="fl icon-calendar_icon-new icon-onward-calendar icon"></span>
-                <div>
-                  <input
-                    type="text"
-                    id="onward_cal"
-                    className="db"
-                    readonly="readonly"
-                    tabindex="3"
-                    data-caleng=""
-                  />
-                  <label for="onward_cal" className="db text-trans-uc">
-                    Date
-                  </label>
-                  <div className="date-error">
-                    "Onward date should be before return date"
-                  </div>
-                </div>
-              </div>
-              <button id="search_btn" className="fl button">
-                Search Buses
-              </button>
+          <form className="form" onSubmit={(e) => onSubmit(e)}>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Enter City"
+                name="from"
+                value={from}
+                onChange={(e) => onChange(e)}
+                required
+              />
             </div>
-          </section>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Enter City"
+                name="password"
+                value={to}
+                onChange={(e) => onChange(e)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="Date"
+                placeholder="Selct the date"
+                name="date"
+                value={date}
+                onChange={(e) => onChange(e)}
+              />
+            </div>
+            <input type="submit" className="btn btn-primary" value="Search" />
+          </form>
         </div>
       </div>
     </section>
